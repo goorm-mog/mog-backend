e7b88929c2ee023e194986e3f86012f9f130e480
lee0806 <leeseh0806@gmail.com>
2026-07-01 15:59:16 +0900

    [MOG-37] DTO 작성
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

---
+++ src/main/java/com/mog/project/domain/summary/dto/response/CardImageResponse.java
@@ -0,0 +1,6 @@
+package com.mog.project.domain.summary.dto.response;
+
+public record CardImageResponse(
+        String cardImageUrl
+) {
+}
---
+++ src/main/java/com/mog/project/domain/summary/dto/response/SummaryParticipantResponse.java
@@ -0,0 +1,7 @@
+package com.mog.project.domain.summary.dto.response;
+
+public record SummaryParticipantResponse(
+        String nickname,
+        Integer amount
+) {
+}
